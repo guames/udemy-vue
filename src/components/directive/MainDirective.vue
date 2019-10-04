47a446edc0c3308b38c1e948a8f7fed5dff635ff
Gustavo Graciano Ames <gustavo.ames@suntech.com.br>
2019-10-04 14:58:11 -0300

    Class 167

--- src/components/directive/MainDirective.vue
+++ src/components/directive/MainDirective.vue
@@ -2,8 +2,16 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Directives</h1>
-
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some text'"></p>
+        <p v-html="'Some <strong>strong</strong> Text'"></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight="'red'"> Color this</p>
+        <p v-highlight:background="'red'"> Color this</p>
       </div>
     </div>
   </div>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from "./App";
 
 export const eventBus = new Vue();
 
+Vue.directive('highlight', {
+  bind(el, binding, vnode){
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    if(binding.arg == 'background'){
+      el.style.backgroundColor = binding.value;
+    }else{
+      el.style.color = binding.value;
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
