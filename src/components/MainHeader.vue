2c3cbd0a5ab0b2786dfc672f2375e7e0eb661107
Gustavo Graciano Ames <gustavo.ames@suntech.com.br>
2019-10-07 14:42:30 -0300

    #237 - Navigation button

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul class="nav nav-pills">
-      <li role="presentation"><router-link to="/">Home</router-link></li>
-      <li role="presentation"><router-link to="/user">User</router-link></li>
+      <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+      <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
     </ul>
   <hr>
   </div>
